d0b3977b2ad59972873b5a0ed174f9157db6477e
feiguiwoxin <33683825+feiguiwoxin@users.noreply.github.com>
2017-12-26 14:16:42 +0800

    增加了从修改过的JS中获取解码结果的方法，现在可以正确的读到各个图片的地址了

--- src/GetComic/GetPicture.java
+++ src/GetComic/GetPicture.java
@@ -1,5 +1,7 @@
 package GetComic;
 
+import java.io.FileReader;
+import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -7,43 +9,82 @@ import javax.script.Invocable;
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
 
+import Start.Start;
+
 public class GetPicture {
-	private String HtmlInfo = null;
-	private String ConfigJS = null;
-	private String ParkerJS = null;
+	private ArrayList<String> PicturePath = new ArrayList<String>();
 	
 	public GetPicture(String UrlAdd)
 	{
-		this.HtmlInfo = GetHtml.GetInfo(UrlAdd);
-		System.out.println(HtmlInfo);
-		GetJsInfo();
+		GetJsInfo(GetHtml.GetInfo(UrlAdd));
 	}
-	
-	private void GetJsInfo()
+	/*从网页内容中获取我们需要用于解码的JS，这里漫画柜使用了2层压缩：
+	 * 1.eval函数本身执行的就是一个parker压缩;
+	 * 2.对于parker压缩的第四个入参也就是k,需要先进行base64解压缩;
+	 * 这个函数的目的就是为了获取解码需要的相关参数*/
+	private void GetJsInfo(String HtmlInfo)
 	{
 		if(null == HtmlInfo) return;
-		String configMath = "<script type=\"text/javascript\" src=\"http://c.3qfm.com/scripts/config_(.+?).js\"></script>";
-		String parkerMatch = "\\(function\\(p,a,c,k,e,d\\)(.+?)</script>";
 		
-		Pattern pattern = Pattern.compile(configMath);
+		String Para1 = null,Para2 = null,Para3 = null,Para4 = null,Para5 = null;
+		String Base64StringMatch = "\\(function\\(p,a,c,k,e,d\\)\\{(.+?)\\}\\('(.+?)',(.+?),(.+?),'(.+?)'(.+?),(.+?),";
+		
+		Pattern pattern = Pattern.compile(Base64StringMatch);
 		Matcher match = pattern.matcher(HtmlInfo);
 		
 		while(match.find())
 		{
-			ConfigJS = match.group(0);
-			System.out.println(ConfigJS);
+			Para1 = match.group(2);
+			Para2 = match.group(3);
+			Para3 = match.group(4);
+			Para4 = match.group(5);
+			Para5 = match.group(7);
 		}
 		
-		pattern = Pattern.compile(parkerMatch);
-		match = pattern.matcher(HtmlInfo);
-		
-		while(match.find())
+		if(null == Para1)
 		{
-			ParkerJS = "<script type=\"text/javascript\">eval" + match.group(0);
-			System.out.println(ParkerJS);
+			return;
 		}
+		
+		paraseInfo(Para1,Integer.parseInt(Para2), Integer.parseInt(Para3), Para4,Integer.parseInt(Para5));
 		
 		return;
 	}
+	//此处直接调用修改自源HTML的脚本来进行解码
+	private void paraseInfo(String para1, int para2, int para3, String para4, int para5)
+	{
+		ScriptEngineManager engineManager = new ScriptEngineManager();
+		ScriptEngine engine = engineManager.getEngineByName("js");
+		try {
+			engine.eval(new FileReader(Start.class.getResource("/config.js").toURI().getPath()));
+			Invocable inv = (Invocable)engine;
+			String ParkerInfo = (String)inv.invokeFunction("parase", para1,para2,para3,para4,para5);
+			getPath(ParkerInfo);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}	
+	}
+	//根据解码结果获取到图片的URL地址
+	private void getPath(String ParkerInfo)
+	{
+		String path = null;
+		String[] files = null;
+		String MatchString = "files\":\\[(.+?)\\],(.+?)path\":\"(.+?)\"";
+		Pattern pattern = Pattern.compile(MatchString);
+		Matcher match = pattern.matcher(ParkerInfo);
+		
+		while(match.find())
+		{
+			path = match.group(3);
+			files = match.group(1).replaceAll("\"","").split(",");
+		}
+		for(String file:files)
+		{
+			PicturePath.add("http://i.hamreus.com:8080" + path + file);
+		}
+	}
 	
+	public ArrayList<String> getPicturePath() {
+		return PicturePath;
+	}
 }
--- src/Start/Start.java
+++ src/Start/Start.java
@@ -4,6 +4,7 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.ArrayList;
 
+import javax.script.Invocable;
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
 import javax.script.ScriptException;
@@ -16,22 +17,21 @@ import GetComic.SaveImg;
 
 public class Start {
 
-	public static void main(String[] args)
+	public static void main(String[] args) throws FileNotFoundException, ScriptException, NoSuchMethodException
 	{
 		//TODO 做成有UI画面的输入框
-		ArrayList<Chapter> result = null;
+//		ArrayList<Chapter> result = null;
+//		
+//		
+//		result = (new GetChapter("16058")).getChapter();
+//		for(Chapter ch:result)
+//		{
+//			System.out.println(ch.getTitle() + "," + ch.getHtml());
+//		}
 		
-		result = (new GetChapter("1231231231231")).getChapter();
-		for(Chapter ch:result)
-		{
-			System.out.println(ch.getTitle() + "," + ch.getHtml());
-		}
+		new GetPicture("http://www.manhuagui.com/comic/8356/340381.html");
 		
-		//new GetPicture("http://www.manhuagui.com/comic/16725/172846.html");
-		
-		//new SaveImg("http://i.hamreus.com:8080/ps1/d/djssg/re/%E7%AC%AC130%E5%9B%9E/2_6313.jpg.webp", "html/", "02.webp").SavePicture();
-		
-
+		//new SaveImg("http://i.hamreus.com:8080/ps1/d/djssg/re/%E7%AC%AC130%E5%9B%9E/2_6313.jpg.webp", "html/", "02.webp").SavePicture();    
 	}
 }
 
